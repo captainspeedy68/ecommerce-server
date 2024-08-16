b0f9cdab84f733da6d4ba66233596ef5d41199d5
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-08-16 20:12:10 +0600

    mongoose express eslint added

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { Application, Request, Response } from "express"
+import cors from "cors";
+import express from "express";
+
+const app: Application = express()
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  const a:string = "hellow"; 
+  res.send(a);
+})
+
+
+export default app;
