491b7e0a81397473dbd4c6c782e110f9e9c0b67f
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-08-24 23:15:32 +0600

    prod changes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
-
+const port = process.env.PORT || 3000
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    app.listen(port, () => {
       // console.log(`Example app listening on port ${config.port}`);
     });
   } catch {
