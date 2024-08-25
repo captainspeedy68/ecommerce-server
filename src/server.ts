3d4ea4bf2d189dc147cf82b16df7d4e51a5b9deb
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-08-25 15:26:44 +0600

    console log for server running

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      // console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example app listening on port ${config.port}`);
     });
-  } catch {
-    // console.log(err);
+  } catch(err) {
+    console.log(err);
   }
 }
 
