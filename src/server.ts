f0a79b001cfaa5d05ad8f5cc4ca7362465c3e46c
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-08-25 12:03:38 +0600

    vercel changes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import config from './app/config';
 import app from './app';
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(port, () => {
+    app.listen(config.port, () => {
       // console.log(`Example app listening on port ${config.port}`);
     });
   } catch {
